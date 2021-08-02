63b8f251032a3640ea68147bb7b99c6e3114fc78
gemmaguilfoyle <83763112+gemmaguilfoyle@users.noreply.github.com>
2021-08-02 21:25:39 +0100

    API Search engine functioning
    OpenWeather API added in JS.
    OpenWeather geolocation API added in JS.
    
    When user searches for a city it triggers update in:
    - City name
    - Temperature
    - Humidity
    - Feels Like
    - Wind
    - Weather condition

--- src/script.js
+++ src/script.js
@@ -34,3 +34,53 @@ if (minutes < 10) {
   minutes = "0" + minutes;
 }
 h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
+
+function showTemperature(response) {
+  let weather = Math.round(response.data.main.temp);
+  let cityName = response.data.name;
+  let displayCityName = document.querySelector("h1");
+  displayCityName.innerHTML = `${cityName}`;
+  let displayWeather = document.querySelector("h4");
+  displayWeather.innerHTML = `${weather}°C`;
+  document.querySelector("h5").innerHTML = response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = `${Math.round(
+    response.data.main.humidity
+  )}%`;
+  document.querySelector("#feelsLike").innerHTML = `${Math.round(
+    response.data.main.feels_like
+  )}°C`;
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )}km/h`;
+}
+
+function searchCity(city) {
+  let apiKey = "1b5c1d1caa03aacc229826f51a319b3a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchBar").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#searchForm");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchlocation(position) {
+  let apiKey = "1b5c1d1caa03aacc229826f51a319b3a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchlocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Cardiff");
