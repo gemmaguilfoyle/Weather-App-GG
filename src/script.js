f2b61bf30519c29c5d60089c27f3bd0f0574a03b
gemmaguilfoyle <83763112+gemmaguilfoyle@users.noreply.github.com>
2021-08-10 12:42:23 +0100

    Added Forecast Section HTML and CSS

--- src/script.js
+++ src/script.js
@@ -1,40 +1,3 @@
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-let currentTime = new Date();
-let h2 = document.querySelector("h2");
-let date = currentTime.getDate();
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let year = currentTime.getFullYear();
-let day = days[currentTime.getDay()];
-let month = months[currentTime.getMonth()];
-if (minutes < 10) {
-  minutes = "0" + minutes;
-}
-h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
-
 function showTemperature(response) {
   let weather = Math.round(response.data.main.temp);
   let cityName = response.data.name;
@@ -75,25 +38,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-let searchForm = document.querySelector("#searchForm");
-searchForm.addEventListener("submit", handleSubmit);
-
-function searchlocation(position) {
-  let apiKey = "1b5c1d1caa03aacc229826f51a319b3a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchlocation);
-}
-
-let currentLocationButton = document.querySelector("#current-location-btn");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("Cardiff");
-
 function displayFahrenheit(event) {
   event.preventDefault();
   let displayWeather = document.querySelector(".largeTemp");
@@ -110,6 +54,61 @@ function displayCelsius(event) {
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
 }
+let searchForm = document.querySelector("#searchForm");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchlocation(position) {
+  let apiKey = "1b5c1d1caa03aacc229826f51a319b3a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchlocation);
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let currentTime = new Date();
+let h2 = document.querySelector("h2");
+let date = currentTime.getDate();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let year = currentTime.getFullYear();
+let day = days[currentTime.getDay()];
+let month = months[currentTime.getMonth()];
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Cardiff");
 
 let celsius = null;
 
